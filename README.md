3dcaaed294ed24396527357fef30639e35cbf3fe
Evgenii <rasshivalov95@mail.ru>
2024-11-03 15:11:09 +0300

    4

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Evgeny111111/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Evgeny111111/frontend-project-46/actions)
+
+#Вычислитель отличий
+
+## Минимальные системные требования
+node.js
+
+## Установка
+make install
+
+## Запуск
+gendiff -h - показать справку
+gendiff путь до файла №1 путь до файла №2
+
+Видео инструкция и пример работы программы со всеми форматами и структурами файлов:
+https://asciinema.org/a/lVbw8fvBXrDuI7wadmC8HQmuY
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,24 +1,19 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import { createRequire } from 'module';
-import { parseFile } from '../src/readFile.js';
-const require = createRequire(import.meta.url);
-const { version } = require('../package.json');
+import genDiff from '../src/genDiff.js'; // Проверьте путь к genDiff
 
 const program = new Command();
 
 program
+  .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .version(version)
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format')
-  .action((filepath1, filepath2, options) => {
-    const data1 = parseFile(filepath1);
-    const data2 = parseFile(filepath2);
+  .action((filepath1, filepath2) => {
+    const diff = genDiff(filepath1, filepath2);
+    console.log(diff);
+  });
 
-    console.log('File 1 data:', data1);
-    console.log('File 2 data:', data2);
-    console.log(`Output format: ${options.format}`);
-  })
-  .parse(process.argv);
+program.parse();
+
