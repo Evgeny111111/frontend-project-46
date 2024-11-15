c1949414c9bc2368baef9d71abb84f3b7aa838e2
Evgenii <rasshivalov95@mail.ru>
2024-11-15 17:51:43 +0300

    fix2

--- src/genDiff.js
+++ src/genDiff.js
@@ -4,6 +4,7 @@ function genDiff(obj1, obj2) {
   const keys1 = _.sortBy(Object.keys(obj1));
   const keys2 = _.sortBy(Object.keys(obj2));
   const unionKeys = _.union(keys1, keys2);
+
   return _.sortBy(unionKeys.map((key) => {
     if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
       return {
@@ -12,14 +13,14 @@ function genDiff(obj1, obj2) {
         value: genDiff(obj1[key], obj2[key]),
       };
     }
-    if (!keys1.includes(key) && keys2.includes(key)) {
+    if (!_.has(obj1, key) && _.has(obj2, key)) { // Заменили условие
       return {
         key,
         type: 'added',
         value: obj2[key],
       };
     }
-    if (keys1.includes(key) && !keys2.includes(key)) {
+    if (_.has(obj1, key) && !_.has(obj2, key)) {
       return {
         key,
         type: 'removed',
--- src/index.js
+++ src/index.js
@@ -4,13 +4,25 @@ import format from './formatters/index.js';
 import genDiff from './genDiff.js';
 import parse from './parsers.js';
 
+/**
+ * Функция для построения абсолютного пути
+ * @param {string} filepath - Путь к файлу (относительный или абсолютный)
+ * @returns {string} Абсолютный путь
+ */
+function resolvePath(filepath) {
+  return path.resolve(process.cwd(), String(filepath));
+}
+
 function parsePaths(filepath1, filepath2, formatter = 'stylish') {
-  const pathResolved1 = path.resolve(process.cwd(), String(filepath1));
+  const pathResolved1 = resolvePath(filepath1);
   const fileContent1 = parse(pathResolved1);
-  const pathResolved2 = path.resolve(process.cwd(), String(filepath2));
+
+  const pathResolved2 = resolvePath(filepath2);
   const fileContent2 = parse(pathResolved2);
+
   const difference = genDiff(fileContent1, fileContent2);
   const result = format(difference, formatter);
+
   return result;
 }
 
