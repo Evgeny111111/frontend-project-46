2f8b038f2bbf8cbc4255cced52c855ac21932c3b
Evgenii <rasshivalov95@mail.ru>
2024-12-04 12:33:52 +0300

    b

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,17 +2,20 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 import JsonFormatter from './JSON.js';
 
-const formatters = {
-  json: JsonFormatter,
-  plain: (difference) => plain(difference).trim(),
-  stylish,
-};
-
 function format(difference, formatName = 'stylish') {
-  const formatter = formatters[formatName];
-  if (!formatter) {
-    throw new Error(`Format name ${formatName} doesn't exist`);
-  }
+  const formatter = (() => {
+    switch (formatName) {
+      case 'stylish':
+        return stylish;
+      case 'plain':
+        return (diff) => plain(diff).trim();
+      case 'json':
+        return JsonFormatter;
+      default:
+        throw new Error(`Format name ${formatName} doesn't exist`);
+    }
+  })();
+
   return formatter(difference);
 }
 
